e926839c70e67d19a3f603d254920e00e248837c
早起喝燕麦 <zhuowangyi@163.com>
2023-04-27 10:40:01 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+
+function handleCount() {
+  count.value++;
+}
+</script>
+
+<template>
+  <div class="page">{{ count }}</div>
+  <div @click="handleCount">点击</div>
+</template>
+
+<style scoped lang="scss">
+.read-the-docs {
+  color: #888;
+}
+</style>
