00dce73d9cc23be483693f946a4013b6f90edf98
早起喝燕麦 <zhuowangyi@163.com>
2023-04-27 16:48:25 +0800

    init

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -2,15 +2,25 @@
   <div>home</div>
 </template>
 
-
 <script>
+console.log([1, 2, 3].map((x) => x * x));
 
-console.log([1,2,3].map(x=> x* x))
+var obj = {
+  p: [
+    "Hello",
+    {
+      y: "World",
+    },
+  ],
+};
 
+var {
+  p: [x, { y }],
+} = obj;
 
+console.log(x, y);
 
-export default({
-  name:'about'
-})
-
+export default {
+  name: "about",
+};
 </script>
