89da5c9f06f48131b40919732829f248e1bb2318
早起喝燕麦 <zhuowangyi@163.com>
2023-05-04 12:00:49 +0800

    init

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -23,4 +23,23 @@ console.log(x, y);
 export default {
   name: "about",
 };
+
+
+let arrayLike = {
+  '0':'a',
+  '1':'b',
+  '2':'c',
+  'length':'3',
+}
+
+
+var arr1 = [].slice.call(arrayLike)
+
+console.log(arr1,'arr1')
+
+let arr2 = Array.from(arrayLike)
+
+console.log(arr2);
+
+
 </script>
